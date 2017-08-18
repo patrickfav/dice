79ee6ca2478d3a38ac23c65a655972796be15e69
Patrick Favre-Bulle <patrick.favrebulle@gmail.com>
2017-08-18 10:54:16 +0200

    Add checkusm config

--- src/main/java/at/favre/tools/dice/RndTool.java
+++ src/main/java/at/favre/tools/dice/RndTool.java
@@ -5,7 +5,7 @@ import at.favre.tools.dice.encode.Loader;
 import at.favre.tools.dice.service.RandomOrgServiceHandler;
 import at.favre.tools.dice.ui.Arg;
 import at.favre.tools.dice.ui.CLIParser;
-import at.favre.tools.dice.util.ByteUtils;
+import org.apache.commons.codec.binary.Base32;
 
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
@@ -31,10 +31,10 @@ public class RndTool {
         byte[] seed;
         SecureRandom secureRandom = new SecureRandom();
         if (arguments.online) {
-            System.out.println("Getting seed from random.org ...");
+            System.out.print("Getting seed from random.org. ");
             seed = new RandomOrgServiceHandler(arguments.debug).getRandom();
             secureRandom.setSeed(seed);
-            System.out.println("Using seed "+ ByteUtils.bytesToHex(seed)+"\n");
+            System.out.println("Using seed " + new Base32().encodeToString(seed) + "\n");
         }
 
         for (Encoder encoder : encoders) {
