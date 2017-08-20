1254f4cbf6bfaa05d312d7679997c850f9e98a82
Patrick Favre-Bulle <patrick.favrebulle@gmail.com>
2017-08-20 11:47:53 +0200

    Add user agent to web service

--- src/main/java/at/favre/tools/dice/service/RandomOrgService.java
+++ src/main/java/at/favre/tools/dice/service/RandomOrgService.java
@@ -4,9 +4,11 @@ import at.favre.tools.dice.service.model.RandomOrgBlobRequest;
 import at.favre.tools.dice.service.model.RandomOrgBlobResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface RandomOrgService {
+    @Headers({"User-Agent: dice/1.0 (github.com/patrickfav/dice)"})
     @POST("/json-rpc/1/invoke")
     Call<RandomOrgBlobResponse> getRandom(@Body RandomOrgBlobRequest request);
 }
