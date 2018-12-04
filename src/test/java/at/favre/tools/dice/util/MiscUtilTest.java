1382e2612830ed3d4d3ab019af40577bc20fb3e9
Patrick Favre-Bulle <patrick.favrebulle@gmail.com>
2018-12-04 23:04:43 +0100

    Fix tests 2

--- src/test/java/at/favre/tools/dice/util/MiscUtilTest.java
+++ src/test/java/at/favre/tools/dice/util/MiscUtilTest.java
@@ -15,8 +15,8 @@ public class MiscUtilTest {
         assertTrue(MiscUtil.isValidPath("/test/test2.txt"));
         assertTrue(MiscUtil.isValidPath("/test/test2/"));
         assertTrue(MiscUtil.isValidPath("good.txt"));
-        assertFalse(MiscUtil.isValidPath("not|good.txt"));
-        assertFalse(MiscUtil.isValidPath("not:good.tx"));
+        assertFalse(MiscUtil.isValidPath("not*good.txt"));
+        assertFalse(MiscUtil.isValidPath("not?good.tx"));
     }
 
     @Test
